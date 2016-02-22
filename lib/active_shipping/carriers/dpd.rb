0bd5afd7df9a0941b5971481f734ccb410d5864e
Charlie Hadden <charlie@builtbybuffalo.com>
2016-02-22 11:33:22 +0000

    Swap street and property

--- lib/active_shipping/carriers/dpd.rb
+++ lib/active_shipping/carriers/dpd.rb
@@ -96,8 +96,8 @@ module ActiveShipping
         json.organisation location.company_name
         json.countryCode location.country.code(:alpha2).value
         json.postcode location.postal_code
-        json.property (location.address2.present? ? location.address1 : location.address2)
-        json.street (location.address2.presence || location.address1)
+        json.street (location.address2.present? ? location.address1 : location.address2)
+        json.property (location.address2.presence || location.address1)
         json.town location.city
         json.county location.province
       end
